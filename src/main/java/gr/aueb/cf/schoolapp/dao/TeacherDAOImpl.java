26dadf9a63949f0957adf7d1f3a684ee5aad8854
Kouvousis <dexous1996@gmail.com>
2024-07-23 21:07:54 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
@@ -0,0 +1,109 @@
+package gr.aueb.cf.schoolapp.dao;
+
+import gr.aueb.cf.schoolapp.dao.exceptios.TeacherDAOException;
+import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.service.util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TeacherDAOImpl implements ITeacherDAO {
+    @Override
+    public Teacher insert(Teacher teacher) throws TeacherDAOException {
+        String sql = "INSERT INTO teachers (firstname, lastname) VALUES (?, ?)";
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            String firstname = teacher.getFirstname();
+            String lastname = teacher.getLastname();
+
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+
+            int n = ps.executeUpdate();
+            // logging
+            return teacher; // TBD
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // logging
+            throw new TeacherDAOException("SQL error in insert teacher" + teacher);
+        }
+    }
+
+    @Override
+    public Teacher update(Teacher teacher) throws TeacherDAOException {
+        String sql = "UPDATE teachers SET firstname = ?, lastname = ?, WHERE id = ?";
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            int id = teacher.getId();
+            String firstname = teacher.getFirstname();
+            String lastname = teacher.getLastname();
+
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+            ps.setInt(3, id);
+
+            int n = ps.executeUpdate();
+            // logging
+            return teacher; // TBD
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // logging
+            throw new TeacherDAOException("SQL error in update teacher" + teacher);
+        }
+    }
+
+    @Override
+    public void delete(Integer id) throws TeacherDAOException {
+        String sql = "DELETE FROM teachers WHERE id = ?";
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            // logging
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // logging
+            throw new TeacherDAOException("SQL error in delete teacher with id: " + id);
+        }
+    }
+
+    @Override
+    public Teacher getById(Integer id) throws TeacherDAOException {
+        String sql = "SELECT * FROM teachers WHERE ID = ?";
+        Teacher teacher = null;
+        ResultSet rs;
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                teacher = new Teacher(
+                        rs.getInt("id"),
+                        rs.getString("firstname"),
+                        rs.getString("lastname")
+                );
+            }
+
+            return teacher;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // logging
+            throw new TeacherDAOException("SQL error in get by id with id: " + id);
+        }
+    }
+
+    @Override
+    public List<Teacher> getByLastName(String lastname) throws TeacherDAOException {
+        return List.of();
+    }
+}
